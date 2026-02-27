e89066ed3970095c70372784f5db47119c0908cc
brandonalas074-maker <brandonalas074@gmail.com>
2026-02-26 21:01:31 -0600

    se crea primer ejercicio de python

---
+++ Pythonpráctica/suma.py
@@ -0,0 +1,60 @@
+# es un comentario
+
+## para declarar una variable
+nombre = "Brandon"
+print(nombre)
+
+edad = 18
+print(nombre, edad, " years old")  ##concatenar
+# binario = 0
+# binario = 1
+
+# espacio de memoria 0,1,2,3,4...
+
+# inmutabilidad : pero siempre deben conservar su tipo
+# strig = letras
+# es un objeto y graba posiciones : 012345
+
+## direccion # 0x
+## tipo => numerico o string (booleano, objeto)
+## tipo numero => prepara a la memoria para
+##guardar un tipo de dato string se hara un hexadecimmal
+
+## textos largos vamos a utilizar las comillas triples
+texto_largo = """
+# binario = 0
+# binario = 1
+
+# espacio de memoria 0,1,2,3,4...
+
+# inmutabilidad : pero siempre deben conservar su tipo
+# strig = letras
+# es un objeto y graba posiciones : 012345
+
+## direccion # 0x
+## tipo => numerico o string (booleano, objeto)
+## tipo numero => prepara a la memoria para
+##guardar un tipo de dato string se hara un hexadecimmal
+
+## textos largos vamos a utilizar las comillas triples
+"""
+print(texto_largo)
+
+## Las comillas triples se usan para textos largos
+## mantienen el formato de lo que se copia entre ellas
+## al momento de compilarlas
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 + numero2
+print("El resultado de la suma es:", resultado)
+
+resultado = numero1 - numero2
+print("El resultado de la suma es:", resultado)
+
+resultado = numero1 / numero2
+print("El resultado de la suma es:", resultado)
+
+resultado = numero1 * numero2
+print("El resultado de la suma es:", resultado)
