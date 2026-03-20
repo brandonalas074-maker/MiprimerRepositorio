bbacff77f1e8366dd5476fa67838a96d47270839
brandonalas074-maker <brandonalas074@gmail.com>
2026-03-19 22:40:04 -0600

    Se hizo el ejercicio 5

---
+++ EjerciciosdePySemana9/Ejercicio05.py
@@ -0,0 +1,6 @@
+# Solicitamos una palabra al usuario
+frase = input("Ingresa una palabra: ")
+
+# Mostraremos la primera y última letra
+print("Primera letra:", frase[0])
+print("Ultima letra:", frase[-1])
