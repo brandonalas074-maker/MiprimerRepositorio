a7e7f2c312feb0ca6c70d521ff4bf93cc4ec49bb
brandonalas074-maker <brandonalas074@gmail.com>
2026-03-19 21:56:45 -0600

    Se hizo el ejercicio 01

---
+++ EjerciciosdePySemana9/Ejercicio01.py
@@ -0,0 +1,11 @@
+# Solicitarle el nombre completo al usuario
+nombre = input("Ingresa tu nombre completo: ")
+
+# Mostrar el nombre en mayúsculas
+print("nombre en mayúsculas:", nombre.upper())
+
+# Mostrar el nombre en minúsculas
+print("nombre en minúsculas:", nombre.lower())
+
+# Cantidad de caracteres que contiene el nombre y incluye también los espacio
+print("cantidad de caracteres:", len(nombre))
