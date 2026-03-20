16fd15d044731371b1b633fc1621f784133cadcc
brandonalas074-maker <brandonalas074@gmail.com>
2026-03-19 23:14:00 -0600

    Se realiza el ejercicio 8

---
+++ EjerciciosdePySemana9/Ejercicio08.py
@@ -0,0 +1,8 @@
+# Solicitamos una frase a nuestro usuario
+frase = input("Ingresa una frase: ")
+
+# Lo que haremos es contar cuántas veces aparece la letra "a"
+cantidad = frase.count("a")
+
+# Mostramos el resultado
+print("La letra 'a' aparece:", cantidad, "veces")
