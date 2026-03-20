e77955f0778970b8fef3bcf51f8d2525ba73629d
brandonalas074-maker <brandonalas074@gmail.com>
2026-03-19 23:21:57 -0600

    Se realiza el ejercicio 9

---
+++ EjerciciosdePySemana9/Ejercicio09.py
@@ -0,0 +1,8 @@
+# Solicitamos una frase al usuario
+frase = input("Ingrese una frase: ")
+
+# Verificaremos si empieza con "Hola"
+if frase.startswith("Hola"):
+    print("La frase empieza con 'Hola'")
+else:
+    print("La frase NO empieza con 'Hola'")
