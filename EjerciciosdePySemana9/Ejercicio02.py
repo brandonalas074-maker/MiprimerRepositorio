987505b903c6bebc7185db9e63794616bce052f0
brandonalas074-maker <brandonalas074@gmail.com>
2026-03-19 22:10:07 -0600

    Se hizo el ejercicio 02

---
+++ EjerciciosdePySemana9/Ejercicio02.py
@@ -0,0 +1,11 @@
+# Solicitaremos una frase al usuario
+frase = input("Ingresa una frase: ")
+
+# Primero mostraremos la frase original
+print("Frase original:", frase)
+
+# Ahora mostramos en mayúsculas
+print("Frase en mayúsculas:", frase.upper())
+
+# Y ahora en minúsculas
+print("Frase en minúsculas:", frase.lower())
