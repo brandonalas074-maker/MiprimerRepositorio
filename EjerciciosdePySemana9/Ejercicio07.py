5059e87cd2bb2277990c32e22ee96b08c3e82ae2
brandonalas074-maker <brandonalas074@gmail.com>
2026-03-19 23:04:14 -0600

    Se ejecuta el ejercicio 7

---
+++ EjerciciosdePySemana9/Ejercicio07.py
@@ -0,0 +1,9 @@
+# Solicitar una frase
+frase = input("Ingresa una frase: ")
+
+# Reemplazaremos una frase por otra, al ser ingresada
+# en este caso "Hola" por "Bye brou"
+nueva_frase = frase.replace("Hola", "Bye brou")
+
+# Mostrar resultado
+print("Frase modificada:", nueva_frase)
