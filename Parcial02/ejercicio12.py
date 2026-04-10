44833f16c0524db4a884596ce80f37c766918d9a
brandonalas074-maker <brandonalas074@gmail.com>
2026-04-10 14:21:06 -0600

    Parcial 02 Fundamentos de programación, ejercicio 12, Toma el nombre de un archivo, quita prefijo y sufijo, y deja el restante en minusculas

---
+++ Parcial02/ejercicio12.py
@@ -0,0 +1,9 @@
+archivo = "BRANDON.txt"
+sin_sufijo = archivo.split(".")[0]
+sin_prefijo = sin_sufijo.split("ING. ")[-1]
+resultado = sin_prefijo.lower()
+
+print("Texto limpio:", resultado)
+
+# Requeria más analisis Inge!!Pero con un poco de investigación pude!!
+##Pronto sere un buen programador, esto apenas comienza... Gracias
