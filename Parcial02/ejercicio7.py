914a3b5695bad7ee8cd536cbfa0a5a532a027d54
brandonalas074-maker <brandonalas074@gmail.com>
2026-04-10 13:18:41 -0600

    Parcial 02 Fundamentos de programación, ejercicio 7, Toma un numero, le da valores a la izquierda, y verifica si el numero termina en 2

---
+++ Parcial02/ejercicio7.py
@@ -0,0 +1,6 @@
+numero = "42"
+rellenadoconcero = numero.zfill(5)
+terminacion_en_2 = rellenadoconcero.endswith("2")
+
+print("Con zfill:", rellenadoconcero)
+print("Termina en '2'?:", terminacion_en_2)
