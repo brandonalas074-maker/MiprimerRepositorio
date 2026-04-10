dbed3673659c71cba74033af6691517e60ba948f
brandonalas074-maker <brandonalas074@gmail.com>
2026-04-10 13:29:53 -0600

    Parcial 02 Fundamentos de programación, ejercicio 8, Verifica cuantas letras a, tiene el texto, y divide las lineas de este exactamente

---
+++ Parcial02/ejercicio8.py
@@ -0,0 +1,9 @@
+Texto = """Es porque un pajarito de la montaña ha hecho 
+en el hueco de un árbol, su nido matinal, que el árbol amanece con
+música en el pecho, como que si tuviera corazón musical"""
+
+cantidad_de_a = Texto.count("a")
+lineas = Texto.splitlines()
+
+print("Cantidad de 'a':", cantidad_de_a)
+print("Lista de líneas:", lineas)
