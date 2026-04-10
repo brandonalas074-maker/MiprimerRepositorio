dfbd48c0cba614ecb6cffdcae39bc529a1443582
brandonalas074-maker <brandonalas074@gmail.com>
2026-04-09 19:53:35 -0600

    Parcial 02 Fundamentos de programación, ejercicio 2,  Reemplaza la palabra Su nombre por Su apellido en el nuevo texto generado

---
+++ Parcial2/Ejercicio2.py
@@ -0,0 +1,6 @@
+frase = "Brandon"
+frase_titulo = frase.title()
+frase_modificada = frase_titulo.replace("Brandon", "Alas")
+
+print("Con title():", frase_titulo)
+print("Con replace():", frase_modificada)
