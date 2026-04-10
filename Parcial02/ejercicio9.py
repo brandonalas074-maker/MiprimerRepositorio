f9acd2ed655473b89b1e2a68bd34341ce9539081
brandonalas074-maker <brandonalas074@gmail.com>
2026-04-10 13:52:22 -0600

    Parcial 02 Fundamentos de programación, ejercicio 9, Realiza la sustitución exacta respetando los espacios y convierte a mayúsculas toda la frase

---
+++ Parcial02/ejercicio9.py
@@ -0,0 +1,6 @@
+cadena = "Any time y Anytime"
+textoreemplazado = cadena.replace("Any time", "Always")
+mayusculas = textoreemplazado.upper()
+
+print("Reemplazado:", textoreemplazado)
+print("En mayusculas:", mayusculas)
