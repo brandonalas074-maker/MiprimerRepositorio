d9a64c2553db6e254b7c2d7d778a55279a7df9c5
brandonalas074-maker <brandonalas074@gmail.com>
2026-04-16 22:10:32 -0600

    Decimo ejercicio

---
+++ Ejerciciosdepractica/10_ejercicio.py
@@ -0,0 +1,10 @@
+usuariocorrecto = "elfather"
+contraseña_correcta = "nose4"
+
+usuario = input("Usuario: ")
+contraseña = input("Contraseña: ")
+
+if usuario == usuariocorrecto and contraseña == contraseña_correcta:
+    print("Acceso permitido")
+else:
+    print("Acceso denegado")
